9cabd05d6cf581ba7f699ddefb332b2128de16f3
Ivan Silvestre <isilvestreneto@gmail.com>
2023-04-05 14:16:16 -0300

    Configuracoes iniciais Spring Security

---
+++ src/main/java/io/github/isilvestreneto/service/impl/UsuarioServiceImpl.java
@@ -0,0 +1,27 @@
+package io.github.isilvestreneto.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		if (!username.equals("ciclano")) {
+			throw new UsernameNotFoundException("Usuário não encontrado na base.");
+		}
+
+		return User.builder().username("ciclano").password(passwordEncoder.encode("123")).roles("USER", "ADMIN")
+				.build();
+	}
+
+}
